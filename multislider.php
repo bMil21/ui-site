3da85ee427e10c3d13f9c37dd7c6ae79e7a206e3
bMil21 <bmil_21@yahoo.com>
2014-05-05 22:31:31 -0400

    multiSlider: Add settings/options

--- multislider.php
+++ multislider.php
@@ -3,14 +3,14 @@
 
 // page vars
 $page_meta = array(
-    "title" => "Multi-Slider | UI Site",
-    "keywords" => "",
-    "description" => ""
+	"title" => "Multi-Slider | UI Site",
+	"keywords" => "",
+	"description" => ""
 );
 $body_class = array(
-    "page" => "pg_multislider",
-    "site_section" => "sct_multislider",
-    "layout" => ""
+	"page" => "pg_multislider",
+	"site_section" => "sct_multislider",
+	"layout" => ""
 );
 // load page styles/scripts
 $css = '<link type="text/css" rel="stylesheet" href="components/multislider/multislider.css" />';
@@ -18,7 +18,7 @@ $scripts = '<script type="text/javascript" src="components/multislider/multislid
 
 include("includes/header.php");
 ?>
-    <div class="wrap">
+	<div class="wrap">
 		<div class="zip"><a href="components/multislider/multislider.zip" title="Download Files">Download Files</a></div> 
 		<h1 class="sect-title">Multi-Slider</h1>
 
@@ -26,7 +26,14 @@ include("includes/header.php");
 		include("components/multislider/multislider.php");
 		?>
 
-    </div>
+	</div>
+	<div class="settings-area">
+		<div class="wrap clearfix">
+			<?php 
+			include("settings/multislider/index.php");
+			?>
+		</div>
+	</div>
 <?php 
 include("includes/footer.php");
 ?>
---
+++ settings/multislider/index.php
@@ -0,0 +1,82 @@
+			<!-- Usage -->
+			<section class="sett-usage">
+				<h2 class="settings-title">HTML</h2>
+				<pre class="mycode"><code>
+&lt;div id=&quot;multi-slider&quot; class=&quot;multi-slider&quot;&gt;
+	&lt;div class=&quot;multi-inner&quot;&gt;
+		&lt;ul class=&quot;slides&quot;&gt;
+			&lt;li class=&quot;slide&quot;&gt;INSERT CONTENT&lt;/li&gt;
+			&lt;li class=&quot;slide&quot;&gt;INSERT CONTENT&lt;/li&gt;
+			&lt;li class=&quot;slide&quot;&gt;INSERT CONTENT&lt;/li&gt;
+		&lt;/ul&gt;
+	&lt;/div&gt;
+&lt;/div&gt;
+				</code></pre>
+			</section>
+
+			<!-- CSS -->
+			<section class="sett-css">
+				<h2 class="settings-title">Default CSS</h2>
+				<pre class="mycode"><code>
+/*-- multiSlider --*/
+.multi-slider{position:relative;max-width:100%;}
+.multi-inner{overflow:hidden;}
+.multi-slider .slides{overflow:hidden;padding:0;margin:0 auto;position:relative;width:1000%;}
+.multi-slider.not-enough .slides{width:auto;}
+.multi-slider .slides > li{display:block;float:left;}
+.multi-slider .slides img{max-width:100%;height:auto;}
+.multi-slider .multi-dirnav{text-align:center;margin:0;}
+.multi-slider .multi-dirlink{display:inline-block;width:30px;height:30px;line-height:30px;background:#5eb8e2;color:#fff;cursor:pointer;position:absolute;top:40%;right:100%;z-index:20;}
+.multi-slider .next{left:100%;right:auto;}
+
+/* User */
+.multi-slider{width:1000px;margin:0 auto;}
+.multi-slider .slides > li{width:250px;text-align:center;}
+
+@media screen and (max-width : 1060px) {
+	.multi-slider{width:750px;
+}
+@media screen and (max-width : 810px) {
+	.multi-slider{width:500px;}
+}
+@media screen and (max-width : 560px) {
+	.multi-slider{width:250px;}
+}
+				</code></pre>
+			</section>
+
+			<!-- jQuery & plugin -->
+			<section class="sett-plugin">
+				<h2 class="settings-title">Add jQuery &amp; Plugin</h2>
+				<pre class="mycode"><code>
+&lt;script src=&quot;js/libs/jquery.js&quot;&gt;&lt;/script&gt;
+&lt;script src=&quot;js/multislider.js&quot;&gt;&lt;/script&gt;
+				</code></pre>
+			</section>
+
+			<!-- Init -->
+			<section class="sett-init">
+				<h2 class="settings-title">Initialize Plugin</h2>
+				<pre class="mycode"><code>
+$(function(){
+	
+	// Init - multiSlider
+	$('#multi-slider').multiSlider();
+	
+});
+				</code></pre>
+			</section>
+
+			<!-- Options -->
+			<section class="sett-options">
+				<h2 class="settings-title">Options</h2>
+				<pre class="mycode"><code>
+$('#multi-slider').multiSlider({
+	// Defaults
+	buildDirNav: true,	// Boolean: Build Arrow Navigation
+	autoPlay: false,	// Boolean: Have slider automatically start or not
+	delay: 5000,		// Integer: Time in between Slides
+	minSlides: 4		// Integer: Minimum number of slides to start slider
+});
+				</code></pre>
+			</section>
