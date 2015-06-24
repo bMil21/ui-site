12ace78f675eec148911af9b2c613cd17d1711ba
Brandon Miller <brandonm@aycmedia.com>
2015-06-24 13:44:02 -0400

    Google Map: send to app/site

--- components/googlemap/googlemap.php
+++ components/googlemap/googlemap.php
@@ -15,6 +15,15 @@
 <!-- Google Map -->
 <h2 class="sect-title">Google Map Helper - Multiple Locations</h2>
 <div id="google-map-adv" class="gmap"></div>
+<!-- Send User to Google Maps, or open up App -->
+<form action="//maps.google.com/maps" method="get" target="new">
+	<input type="hidden" name="daddr" value="INSERT_ADDRESS_OF_LOCATION">
+	<input type="hidden" name="hl" value="en">
+	<p class="inline-submit clearfix">
+		<input type="text" name="saddr2" id="saddr2" style="width:360px;margin-right:20px;" value="" placeholder="e.g. 123 Main St., City, State">
+		<input class="submit" type="submit" value="Get Directions">
+	</p>
+</form>
 <!-- END Google Map -->
 
 <!-- GOOGLE MAPS API - (include in footer of specific page) -->
